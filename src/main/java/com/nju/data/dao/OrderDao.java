3bf1613e7631fb434e5a17974a3339ebe8e442db
sucrey <727844658@qq.com>
2015-05-30 10:49:15 +0800

    Revert "订单详情查看"

--- src/main/java/com/nju/data/dao/OrderDao.java
+++ src/main/java/com/nju/data/dao/OrderDao.java
@@ -1,10 +1,12 @@
 package com.nju.data.dao;
 
+import java.util.List;
+
 import com.nju.data.dataobject.OrderDO;
 
 public interface OrderDao {
+
+	public boolean addOrder(OrderDO orderDO);
 	
-	public void save(OrderDO order);
-	
-	public OrderDO findOrderById(int orderId);
+	public List<OrderDO> getOrderByStaffIdandState(long staffId, int[] states);
 }
--- src/main/java/com/nju/data/dao/impl/OrderDaoImpl.java
+++ src/main/java/com/nju/data/dao/impl/OrderDaoImpl.java
@@ -2,54 +2,21 @@ package com.nju.data.dao.impl;
 
 import java.util.List;
 
-
-
-
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
-
 import com.nju.data.dao.OrderDao;
 import com.nju.data.dataobject.OrderDO;
-import com.nju.data.dataobject.UserDO;
- 
-public class OrderDaoImpl extends HibernateDaoSupport implements OrderDao {
 
-	protected static Logger log = LoggerFactory.getLogger(OrderDao.class);
-	
+public class OrderDaoImpl implements OrderDao{
+
 	@Override
-	public void save(OrderDO order) {
-		log.debug("saving Order instance");
-		try {
-			getHibernateTemplate().save(order);
-			log.debug("save successful");
- 		} catch (RuntimeException re) {
-			log.error("save failed", re);
-			throw re;
-		}
+	public boolean addOrder(OrderDO orderDO) {
+		// TODO Auto-generated method stub
+		return false;
 	}
-	
-	public void find() {
-		String sql = "from OrderDO";
-		Session se = this.currentSession();
-	
-		Query q = se.createQuery(sql);
-		List<UserDO> result = q.list();
-		System.out.println(result.get(0));
 
+	@Override
+	public List<OrderDO> getOrderByStaffIdandState(long staffId, int[] states) {
+		// TODO Auto-generated method stub
+		return null;
 	}
-	
-	@SuppressWarnings("unchecked")
-	public OrderDO findOrderById(int orderId) {				
-		String sql = "from OrderDO where order_id = '"+orderId+"'";
-		Session se = this.currentSession();	
-		Query q = se.createQuery(sql);
-		List<OrderDO> result = q.list();
-		System.out.println(result.get(0));
-		return result.size()==0?null:result.get(0);
-	}
-	
 
 }
